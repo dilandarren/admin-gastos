f2dba4dec9b0d2954c8ea191aedfe844022c718c
Dilan <dilandarren1@gmail.com>
2024-09-30 21:31:15 +0200

    add gastosFiltrados

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
   import Presupuesto from './components/Presupuesto.vue';
-  import { ref, reactive, watch } from 'vue';
+  import { ref, reactive, watch, computed } from 'vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
   import Filtros from './components/Filtros.vue';
@@ -119,6 +119,14 @@
     
   }
 
+  const gastosFiltrados = computed(() => {
+    if (filtro.value) {
+      return gastos.value.filter(gasto => gasto.categoria === filtro.value)
+    }
+
+    return gastos.value
+  })
+
 
 
 </script>
@@ -153,11 +161,12 @@
         />
 
         <div class="listado-gastos contenedor">
-          <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+          <h2>{{ gastosFiltrados.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
 
+           
 
           <Gasto 
-            v-for="gasto in gastos"
+            v-for="gasto in gastosFiltrados"
             :key="gasto.id"
             :gasto="gasto"
             @seleccionar-gasto="seleccionarGasto"
